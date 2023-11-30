4138de9f8993e4b6d7bf05a5dbc4723141609a08
srijan393 <146333937+srijan393@users.noreply.github.com>
2023-11-30 12:26:20 +0000

    Add files via upload

---
+++ week3/task2.py
@@ -0,0 +1,7 @@
+age = 25
+is_under_18 = age < 18
+is_under_21 = age < 21
+is_under_31 = age < 31
+print("Is the age under 18?", is_under_18)
+print("Is the age under 21?", is_under_21)
+print("Is the age under 31?", is_under_31)
